a26d0e15dbe89183a79dd04c5b6b45cbff0700f0
Fridah Kanario <fkanario@tracom.co.ke>
2019-04-10 12:42:08 +0300

    Added Redux and Data tables

--- Client/src/app/layout/dashboard/dashboard.module.ts
+++ Client/src/app/layout/dashboard/dashboard.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbCarouselModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-import {
-    TimelineComponent,
-    NotificationComponent,
-    ChatComponent
-} from './components';
 import { StatModule } from '../../shared';
 import { counterReducer} from './reducer';
 import {StoreModule} from '@ngrx/store';
@@ -24,9 +18,6 @@ import {StoreModule} from '@ngrx/store';
     ],
     declarations: [
         DashboardComponent,
-        TimelineComponent,
-        NotificationComponent,
-        ChatComponent
     ]
 })
 export class DashboardModule {}
